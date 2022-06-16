1f340fd1b59963b670edc9c075bd3d1284d7372b
Prathap <prathap.malempati@gmail.com>
2022-06-16 22:21:29 +0530

    Initial Commit

---
+++ MileCodeChallenge/MileCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileCodeChallengeApp.swift
+//  MileCodeChallenge
+//
+//  Created by Prathap on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MileCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
